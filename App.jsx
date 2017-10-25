240bffa5b2c91102c4b5d349c51a9c90ff388a63
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2017-10-25 00:35:22 -0300

    Add pre commit hook.

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is a test</Text>
+      </View>
+    );
+  }
+}
+
